d36905fd77700f6439ce38703cdce91d6b75dd61
shams <shams.orakzai.777@gmail.com>
2022-02-21 00:53:02 +0500

    Postgres database configured to the project.

--- djano_home_assessment/settings.py
+++ djano_home_assessment/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djano_home_assessment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "Django_Assessment",
+        "USER": "assessment",
+        "PASSWORD": "My_Django_Project",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
