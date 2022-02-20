665c5f9609d8963620eff8526929b3e2e5ad9ddd
shams <shams.orakzai.777@gmail.com>
2022-02-21 02:48:14 +0500

    Sub-application (employee) initialized and Employee model is defined.

---
+++ employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    job = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    hire_date = models.DateField()
+
+
